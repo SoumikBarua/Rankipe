9a6d58b46147ab0f98c86b3e080d59fb26259a87
Soumik Barua <10300902+SoumikBarua@users.noreply.github.com>
2020-12-07 16:30:21 -0500

    Add & install Pod, lay out Storyboards with VCs

---
+++ Pods/Target Support Files/Pods-Rankipe/Pods-Rankipe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Rankipe : NSObject
+@end
+@implementation PodsDummy_Pods_Rankipe
+@end
---
+++ Rankipe/SearchRecipeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchRecipeViewController.swift
+//  Rankipe
+//
+//  Created by SB on 12/7/20.
+//  Copyright © 2020 group10. All rights reserved.
+//
+
+import UIKit
+
+class SearchRecipeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
