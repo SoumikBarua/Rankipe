34c2cf69dfe93f49de31fff4811092d40ba120a5
Soumik Barua <10300902+SoumikBarua@users.noreply.github.com>
2020-12-10 21:18:02 -0500

    Set up basic table cells with videos embedded

--- Rankipe/SearchRecipeViewController.swift
+++ Rankipe/SearchRecipeViewController.swift
@@ -7,16 +7,19 @@
 //
 
 import UIKit
+import youtube_ios_player_helper
 
 class SearchRecipeViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var searchTableView: UITableView!
+    var videoIDs = ["jPOxWOE-3Xk", "DaeQ2AKp00k", "sH-juTnjm-Y", "UBMqcTHHE8Y", "8UkNOQDTb7c", "AoPqKq4Wv_E", "_P1n4KaU_rQ", "mkz8otyYuRE", "isNqwzobpDE", "mcHddbql5pw"]
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         searchTableView.dataSource = self
         searchTableView.delegate = self
+        searchTableView.rowHeight = 118
     }
     
 
@@ -27,11 +30,17 @@ class SearchRecipeViewController: UIViewController, UITableViewDataSource, UITab
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let title = "Cell #\(indexPath.row)"
         let cell = UITableViewCell()
-        cell.textLabel?.text = title
-        
+        let subview = YTPlayerView(frame: CGRect(x: 8, y: 8, width: 100, height: 100))
+        subview.load(withVideoId: videoIDs[indexPath.row])
+        cell.addSubview(subview)
+        cell.heightAnchor.constraint(equalToConstant: 116)
         return cell
+    }
+    
+    // MARK: - Table view delegate methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
     }
 
 }
